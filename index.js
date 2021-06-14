8c3aae1efc9191113c319004c89750efd7560306
franzmongin <franz.mongin@gmail.com>
2021-06-14 19:08:44 +0200

    finished validation

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ let errorMessages = {
     firstName : "Veuillez entrer 2 caractères ou plus pour le champ du prénom",
     lastName : "Veuillez entrer 2 caractères ou plus pour le champ du nom",
     email: "Veuillez entrer une adresse email valide",
-    quantity : "Veuillez entrer un nombre de concours auxquels vous avez participé",
+    quantity : "Veuillez entrer un nombre valide de concours auxquels vous avez participé",
     location :"Veuillez sélectionner une ville",
     conditions : "Veuillez accepter les conditions"
 }
+
+let formValid = false;
 
 // firstName validation
 function checkFirstName(){
     if(/\w{2,}/.test(document.getElementById('first-name').value)){
         document.getElementById('first-name-validation').innerText = "";
+        formValid = true;
 
     }else{
         document.getElementById('first-name-validation').innerText = errorMessages.firstName;
+        formValid = false;
     }
 }
 
@@ -21,19 +25,62 @@ function checkFirstName(){
 function checkLastName(){
     if(/\w{2,}/.test(document.getElementById('last-name').value)){
         document.getElementById('last-name-validation').innerText = "";
+        formValid = true;
 
     }else{
         document.getElementById('last-name-validation').innerText = errorMessages.lastName;
+        formValid = false;
     }
 }
 
 //email validation
 function checkEmail(){
-    if(/^\w+@\w+\.\w{1,3}$/.test(document.getElementById('email').value)){
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)$/.test(document.getElementById('email').value)){
         document.getElementById('email-validation').innerText = "";
+        formValid = true;
     }else{
-        console.log('ok')
         document.getElementById('email-validation').innerText = errorMessages.email;
+        formValid = false
+    }
+}
+
+//quantity validation
+function checkQuantity(){
+    if(/^[0-9]+$/.test(document.getElementById('quantity').value)){
+        document.getElementById('quantity-validation').innerText = "";
+        formValid = true;
+    }else{
+        document.getElementById('quantity-validation').innerText = errorMessages.quantity;
+        formValid = false;
+    }
+}
+
+// location validation
+function checkLocation(){
+    let valid = false;
+    let locations = document.getElementsByName('location');
+    for( let location of locations){
+        if(location.checked){
+            valid = true;
+        }
+    }
+    if(valid){
+        document.getElementById('location-validation').innerText = "";
+        formValid = true;
+    }else{
+        document.getElementById('location-validation').innerText = errorMessages.location;
+        formValid = false;
+    }
+}
+
+// conditions validation
+function checkConditions(){
+    document.getElementById('checkbox1').checked ? (document.getElementById('conditions-validation').innerText = "") : document.getElementById('conditions-validation').innerText = errorMessages.conditions;
+    if(document.getElementById('checkbox1').checked){
+        document.getElementById('conditions-validation').innerText = "";
+        formValid = true
+    }else{
+        formValid = false;
     }
 }
 
@@ -42,6 +89,15 @@ function onSubmit(e){
     checkFirstName();
     checkLastName();
     checkEmail();
+    checkQuantity();
+    checkLocation();
+    checkConditions();
+    if(formValid){
+        document.getElementById('form-confirmation').innerText = "Merci ! Votre réservation a été reçue.";
+    }else{
+        document.getElementById('form-confirmation').innerText = "";
+
+    }
 }
 
 document.getElementById('modal-form').addEventListener("submit", onSubmit)
